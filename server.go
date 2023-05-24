13c4c6c47b1649b52a03dba7c30c728c91b8f3da
Carlos <carlosokumu254@gmail.com>
2023-05-24 22:38:53 +0300

    housekeeping

--- server.go
+++ server.go
@@ -10,42 +10,12 @@ import (
 )
 
 func main() {
-	// var then variable name then variable type
-	//var first string
-	// initServer()
+
 	dsn := "host=localhost user=postgres password=Agent047 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	persistence.Connect(dsn)
-	// persistence.Migrate()
-	// len := len(persistence.GetAllSwingAssets())
-	// fmt.Println("len:", len)
-
-	//badgerconnection := persistence.CreateBadgerConnection()
-
-	//persistence.InsertSymbolData()
-	// symboldata, err := persistence.ReadSymbolData(int64(1))
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println("symbolhere", symboldata)
-
-	//lightsymbol, _ := persistence.ReadLightSymbolData(int64(4))
-	// symboldata, _ := persistence.ReadSymbolData(int64(2))
-
-	// // // if err != nil {
-	// // // 	fmt.Println(err)
-	// // // }
-	//fmt.Println("LightHere:", lightsymbol)
-	// fmt.Println("symboldata:", symboldata)
 
 	router := initServer()
 	router.Run(":" + "8080")
-	// 	var second string
-	// 	fmt.Scanln(&second)
-	// 	fmt.Scanln(&first)
-	// hub := middlewares.NewHub()
-	// go hub.Run()
-
-	// messagehandler.ConnectToOpen("demo.ctraderapi.com", 5035, hub)
 
 	var second string
 	fmt.Scanln(&second)
@@ -62,7 +32,3 @@ func initServer() *gin.Engine {
 	})
 	return router
 }
-
-// 	// return router
-
-// }
