3cccf283d719c35a070214f46ff3a0b931bc31fd
Carlos <carlosokumu254@gmail.com>
2023-10-31 13:44:09 +0300

    comment out -postgres connection

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"ctraderapi/messagehandler"
 	"ctraderapi/middlewares"
-	"ctraderapi/persistence"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +11,11 @@ func main() {
 
 	//dsn := os.Getenv("DATABASE_URL")
 
-	dsn := "postgres://carlos:vUUYROlx74jmAdnvVunkNqiNdxAvZI32@dpg-cg9il4pmbg54mbfbrte0-a.oregon-postgres.render.com/swings"
-	persistence.Connect(dsn)
+	// dsn := "postgres://carlos:vUUYROlx74jmAdnvVunkNqiNdxAvZI32@dpg-cg9il4pmbg54mbfbrte0-a.oregon-postgres.render.com/swings"
+	// persistence.Connect(dsn)
+	//persistence.Migrate()
 	router := initServer()
-	router.Run(":" + "8080")
+	router.Run(":" + "8070")
 
 }
 
@@ -26,7 +26,7 @@ func initServer() *gin.Engine {
 
 	//[Websocket] Echo Endpoint ------
 	router.GET("/ws", func(c *gin.Context) {
-		messagehandler.ConnectToOpen("demo.ctraderapi.com", 5035, hub, c.Writer, c.Request)
+		messagehandler.ConnectToOpen("live.ctraderapi.com", 5035, hub, c.Writer, c.Request)
 	})
 	return router
 }
